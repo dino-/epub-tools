04b0420b095d7de15dfcf5cb95232b69d582d3f4
Dino Morelli <dino@ui3.info>
2008-12-07 05:03:09 +0000

    Added a lot of tests for simple books
    Also factored out the assertion that a set of metadata fields from a book make a certain filename string.

--- testsuite/Test/BookName/Format/Simple.hs
+++ testsuite/Test/BookName/Format/Simple.hs
@@ -8,27 +8,84 @@ module Test.BookName.Format.Simple (
    where
 
 import Test.HUnit ( Test (..), assertEqual )
+import Test.HUnit.Base ( Assertion )
 
 import BookName.Extract
 import BookName.Format.Simple
 import BookName.Util
 
 
+assertNewName :: String -> [String] -> String -> Assertion
+assertNewName desc meta expected = do
+   let fields = parseMeta "foo" $ unlines meta
+   result <- runBN $ formatSimple fields
+   let actual = either id id result
+   assertEqual desc expected actual
+
+
 testSimpleAll :: Test
 testSimpleAll = TestList
-   [ testSimpleWithThe
+   [ testNoAuthor
+   , testNoTitle
+   , testAllPunctuation
+   , testWithThe
+   , testAuthorSt
    ]
 
 
-testSimpleWithThe :: Test
-testSimpleWithThe = TestCase $ do
-   let lrfMeta = unlines
+testNoAuthor :: Test
+testNoAuthor = TestCase $
+   assertNewName "testNoAuthor" lrfMeta expected
+   where
+      lrfMeta =
+         [ "FreeText: Doesn't matter"
+         , "Title: Insignificant"
+         ]
+      expected = "[ERROR missing key: Author]"
+
+
+testNoTitle :: Test
+testNoTitle = TestCase $
+   assertNewName "testNoTitle" lrfMeta expected
+   where
+      lrfMeta =
+         [ "Author: Nobody McCrankypants"
+         , "FreeText: Doesn't matter"
+         ]
+      expected = "[ERROR missing key: Title]"
+
+
+testAllPunctuation :: Test
+testAllPunctuation = TestCase $
+   assertNewName "testAllPunctuation" lrfMeta expected
+   where
+      lrfMeta =
+         [ "Author: Dino Morelli"
+         , "FreeText: 2008 Dino Morelli"
+         , "Title: The *crazy*: Sand-box. Of Smedley's discontent, fear & Malnourishment? (Maybe not!); [Part #2]"
+         ]
+      expected = "MorelliDino-CrazySandBoxOfSmedleysDiscontentFearAndMalnourishmentMaybeNot_Part2_2008.lrf"
+
+
+testWithThe :: Test
+testWithThe = TestCase $
+   assertNewName "testWithThe" lrfMeta expected
+   where
+      lrfMeta =
          [ "Author: Kate Wilhelm"
          , "FreeText: 1974 Kate Wilhelm"
          , "Title: The Hounds"
          ]
-   let fields = parseMeta "foo" lrfMeta
-   result <- runBN $ formatSimple fields
-   let actual = either id id result
-   assertEqual "testSimpleWithThe"
-      "WilhelmKate-Hounds_1974.lrf" actual
+      expected = "WilhelmKate-Hounds_1974.lrf"
+
+
+testAuthorSt :: Test
+testAuthorSt = TestCase $
+   assertNewName "testAuthorSt" lrfMeta expected
+   where
+      lrfMeta =
+         [ "Author: Jennifer St. Clair"
+         , "FreeText: 2004, Jennifer St. Clair"
+         , "Title: Budget Cuts"
+         ]
+      expected = "StClairJennifer-BudgetCuts_2004.lrf"
