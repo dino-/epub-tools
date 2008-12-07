be6d539ed194008fb89f39282cd44811b54a41ad
Dino Morelli <dino@ui3.info>
2008-12-07 04:26:22 +0000

    More big changes to this test module
    This is incomplete, next record will move it to proper subdir.

--- testsuite/TestSimple.hs
+++ testsuite/TestSimple.hs
@@ -2,53 +2,33 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Dino Morelli <dino@ui3.info>
 
-module TestSimple (
-   testSimple
+module Test.BookName.Format.Simple (
+   testSimpleAll
 )
    where
 
-import Test.HUnit ( Test (..), assertBool, assertString )
---import Text.Regex.Posix ( (=~) )
+import Test.HUnit ( Test (..), assertEqual )
+
+import BookName.Extract
+import BookName.Format.Simple
+import BookName.Util
 
 
-{- Test foo bar baz
--}
-
-
-testSimple :: Test
-testSimple = TestList
-   [ TestLabel "testOne" testOne
+testSimpleAll :: Test
+testSimpleAll = TestList
+   [ testSimpleWithThe
    ]
 
 
-testOne :: Test
-testOne = TestCase $ assertBool "testOne description" False
-
-
-{-
-topDir = Util.resourcesPath </> "foo"
-oldPath = Util.resourcesPath </> "img_1220.jpg"
-newLinkPath = topDir </> "2003/2003-09-02/20030902_220.jpg"
-
-
-testLink :: Test
-testLink = TestCase $ do
-   -- Run the program with known input data
-   (output, procH) <- Util.getBinaryOutput [ topDir, oldPath ]
-   waitForProcess procH
-
-   -- Check that the correct output path exists
-   existsNew <- fileExist newLinkPath
-   assertBool "make link: existance of new link" existsNew
-
-   -- Check that old path still exists
-   existsOld <- fileExist oldPath
-   assertBool "make link: existance of old link" existsOld
-
-   -- Remove files and dirs that were created
-   removeDirectoryRecursive topDir
-
-   -- Test output to stdout
-   assertBool "make link: correct output"
-      (output =~ newLinkPath :: Bool)
--}
+testSimpleWithThe :: Test
+testSimpleWithThe = TestCase $ do
+   let lrfMeta = unlines
+         [ "Author: Kate Wilhelm"
+         , "FreeText: 1974 Kate Wilhelm"
+         , "Title: The Hounds"
+         ]
+   let fields = parseMeta "foo" lrfMeta
+   result <- runBN $ formatSimple fields
+   let actual = either id id result
+   assertEqual "testSimpleWithThe"
+      "WilhelmKate-Hounds_1974.lrf" actual
