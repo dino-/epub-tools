c0b41f228304360189a14de1decf4521281b5101
Dino Morelli <dino@ui3.info>
2011-01-09 17:24:11 +0000

    Removed unused Fields data type in the BookName.Util common code

--- src/BookName/Util.hs
+++ src/BookName/Util.hs
@@ -6,19 +6,13 @@
 
 module BookName.Util
    ( runBN
-   , Fields
    )
    where
 
 import Control.Monad.Error
-import Data.Map
 
 
 --type BN a = (ErrorT String IO) a
 
-
 runBN :: (ErrorT e m) a -> m (Either e a)
 runBN = runErrorT
-
-
-type Fields = Map String String
