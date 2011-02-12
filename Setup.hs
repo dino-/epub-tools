97e39b6ecd76ea9626cb1d4da682557fcda18ac5
Dino Morelli <dino@ui3.info>
2011-02-12 17:54:13 +0000

    Removed code in Setup.hs to make a symlink to the binary
    Working on eliminating anything that's not Windows-compatible from this project.

--- Setup.hs
+++ Setup.hs
@@ -4,29 +4,15 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Dino Morelli <dino@ui3.info>
 
-import Control.Monad ( unless )
 import Distribution.Simple
-import System.Cmd
-import System.FilePath
-import System.Posix.Files ( createSymbolicLink, fileExist )
+import System.Cmd ( system )
 
 
 main = defaultMainWithHooks (simpleUserHooks 
-   { postBuild = customPostBuild
-   , runTests = testRunner
+   { runTests = testRunner
    } )
 
    where
-      -- Create symlink to the binary after build for developer 
-      -- convenience
-      customPostBuild _ _ _ _ = do
-         let dest = "epubname"
-
-         exists <- fileExist dest
-         unless exists $ do
-            let src = "dist" </> "build" </> dest </> dest
-            createSymbolicLink src dest
-
       -- Target for running all unit tests
       testRunner _ _ _ _ = do
          system $ "runhaskell -isrc testsuite/runtests.hs"
