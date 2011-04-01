64978d3fb94701a8c888020b929bb04723398888
Dino Morelli <dino@ui3.info>
2011-04-01 20:44:13 +0000

    Modifications to the Opts code to include new switches and types
    Also made parsing opts an action in the EM monad transformer (ErrorT and IO)

--- src/EpubTools/EpubMeta/Opts.hs
+++ src/EpubTools/EpubMeta/Opts.hs
@@ -3,16 +3,48 @@
 -- Author: Dino Morelli <dino@ui3.info>
 
 module EpubTools.EpubMeta.Opts
-   ( Options (..)
+   ( Edit (..)
+   , Export (..)
+   , Options (..)
    , parseOpts, usageText
    )
    where
 
 import System.Console.GetOpt
 
+import EpubTools.EpubMeta.Util
+
+
+-- There are three states for this, representing with a new type
+data Edit
+   = Backup String
+   | NoBackup
+   | NotEditing
+
+
+maybeToEdit :: Maybe String -> Edit
+maybeToEdit (Just suffix) = Backup suffix
+maybeToEdit Nothing       = NoBackup
+
+
+-- There are three states for this, representing with a new type
+data Export
+   = ToPath String   -- User specified a file to export to
+   | Existing        -- User wants export to use existing name
+   | NoExport        -- This arg wasn't used at all
+   deriving Eq
+
+
+maybeToExport :: Maybe String -> Export
+maybeToExport (Just file) = ToPath file
+maybeToExport Nothing     = Existing
+
 
 data Options = Options
    { optHelp :: Bool
+   , optEdit :: Edit
+   , optImport :: Maybe String
+   , optExport :: Export
    , optVerbose :: Bool
    }
 
@@ -20,6 +52,9 @@ data Options = Options
 defaultOptions :: Options
 defaultOptions = Options
    { optHelp = False
+   , optEdit = NotEditing
+   , optImport = Nothing
+   , optExport = NoExport
    , optVerbose = False
    }
 
@@ -29,17 +64,26 @@ options =
    [ Option ['h'] ["help"] 
       (NoArg (\opts -> opts { optHelp = True } ))
       "This help text"
+   , Option ['e'] ["edit-opf"]
+      (OptArg (\f opts -> opts { optEdit = maybeToEdit f }) "SUF")
+      "Edit a book's OPF XML data in a text editor. If an optional suffix is supplied, a backup of the book will be created with that suffix"
+   , Option ['x'] ["export"]
+      (OptArg (\f opts -> opts { optExport = maybeToExport f }) "FILE")
+      "Export the book's OPF XML metadata to a file. If no file name is given, the name of the file in the book will be used"
+   , Option ['i'] ["import"]
+      (ReqArg (\f opts -> opts { optImport = Just f }) "FILE")
+      "Import OPF metadata from the supplied XML file"
    , Option ['v'] ["verbose"]
       (NoArg (\opts -> opts { optVerbose = True } )) 
       "Display all OPF package info, including manifest, spine and guide"
    ]
 
 
-parseOpts :: [String] -> IO (Options, [String])
+parseOpts :: [String] -> EM (Options, [String])
 parseOpts argv = 
    case getOpt Permute options argv of
       (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
-      (_,_,errs) -> ioError $ userError (concat errs ++ usageText)
+      (_,_,errs) -> throwError $ concat errs ++ usageText
 
 
 usageText :: String
@@ -47,10 +91,14 @@ usageText = (usageInfo header options) ++ "\n" ++ footer
    where
       header = init $ unlines
          [ "Usage: epubmeta [OPTIONS] EPUBFILE"
-         , "Examine EPUB OPF package data"
+         , "View or edit EPUB OPF package data"
          , ""
          , "Options:"
          ]
       footer = init $ unlines
-         [ "Version 1.0.0.0  Dino Morelli <dino@ui3.info>"
+         [ "When -v or no options are given, epubmeta will display the OPF package data in a human-readable form."
+         , ""
+         , "The -e feature will look for an editor in this order: the EDITOR env var, the VISUAL env var, /usr/bin/vi"
+         , ""
+         , "Version 1.0.0.0  Dino Morelli <dino@ui3.info>"
          ]
