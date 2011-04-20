d5b325eb0689c4af89e197c16a34c4f7aa47720d
Dino Morelli <dino@ui3.info>
2011-04-20 21:06:41 +0000

    Missed changing the formatter module identification string

--- src/EpubTools/EpubName/Format/MagClarkesworld.hs
+++ src/EpubTools/EpubName/Format/MagClarkesworld.hs
@@ -14,7 +14,7 @@ import EpubTools.EpubName.Util
 
 
 fmtMagClarkesworld :: Metadata -> EN (String, String)
-fmtMagClarkesworld = format "MagApex"
+fmtMagClarkesworld = format "MagClarkesworld"
    ".*" (const "")
    "^(Clarkesworld)[^0-9]*([0-9]+)$" title
 
