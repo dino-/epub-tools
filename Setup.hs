aa55a4843a7fa585b5271d9c6da76d362bcc21a6
Dino Morelli <dino@ui3.info>
2011-10-27 02:16:20 +0000

    Added stock Setup.hs to project, required even if we don't use it

---
+++ Setup.hs
@@ -0,0 +1,10 @@
+#! /usr/bin/env runhaskell
+
+-- Copyright: 2008, 2011 Dino Morelli
+-- License: BSD3 (see LICENSE)
+-- Author: Dino Morelli <dino@ui3.info>
+
+import Distribution.Simple
+
+
+main = defaultMain
