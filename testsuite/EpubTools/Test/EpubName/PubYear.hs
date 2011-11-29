89bb6fcee6672f50b6e939134a56cd2d2050eb85
Dino Morelli <dino@ui3.info>
2011-11-29 18:08:30 +0000

    Added another date pattern

--- testsuite/EpubTools/Test/EpubName/PubYear.hs
+++ testsuite/EpubTools/Test/EpubName/PubYear.hs
@@ -23,6 +23,7 @@ pubYearTests = TestLabel "PubYear" $ TestList
    , testExtractYear "2010-02-14T21:47:47.682882+00:00" "2010"
    , testExtractYear "2011-11-16T09:19:50-0500" "2011"
    , testExtractYear "2011-11-16T14:19:52Z" "2011"
+   , testExtractYear "Tue, 29 Nov 2011 06:21:19 +0000" "2011"
    ]
 
 
