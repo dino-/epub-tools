ee9601c837539049aae629e650d287b2e0cdd158
Dino Morelli <dino@ui3.info>
2012-06-09 18:07:38 +0000

    Added enter to prompt for performing default action

--- src/EpubTools/EpubName/Prompt.hs
+++ src/EpubTools/EpubName/Prompt.hs
@@ -22,12 +22,13 @@ prompt = do
    putStr "\n"
 
    case (toLower c) of
-      'y' -> return Yes
-      ' ' -> return Yes
-      'n' -> return No
-      'q' -> return Quit
-      '?' -> more >> prompt
-      _   -> putStrLn "Unknown choice" >> prompt
+      'y'  -> return Yes
+      ' '  -> return Yes
+      '\n' -> return Yes
+      'n'  -> return No
+      'q'  -> return Quit
+      '?'  -> more >> prompt
+      _    -> putStrLn "Unknown choice" >> prompt
 
 
 continue :: PromptResult -> Bool
@@ -40,5 +41,5 @@ more = putStr . unlines $
    [ "y: rename this file"
    , "n: don't rename this file"
    , "q: cancel now, don't rename any files beyond this point"
-   , "<space>: accept the current default (which is capitalized)"
+   , "<space> or <enter>: accept the default (which is capitalized)"
    ]
