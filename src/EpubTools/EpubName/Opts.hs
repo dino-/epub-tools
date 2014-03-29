534363ecc2045c26e12db4b4ad141abbd3bacd4c
Dino Morelli <dino@ui3.info>
2014-03-29 14:25:56 +0000

    Changed version to 2.5

--- src/EpubTools/EpubName/Opts.hs
+++ src/EpubTools/EpubName/Opts.hs
@@ -219,5 +219,5 @@ usageText = return $ (usageInfo header options) ++ "\n" ++ footer
          , "   epub3: http://www.idpf.org/epub/30/spec/epub30-publications.html"
          , ""
          , ""
-         , "Version 2.4  Dino Morelli <dino@ui3.info>"
+         , "Version 2.5  Dino Morelli <dino@ui3.info>"
          ]
