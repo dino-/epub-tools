ff16dfd27d9ef72a46c1f73341274be1b0089024
Dino Morelli <dino@ui3.info>
2024-03-15 10:41:53 -0400

    Changed from 3-space to 2-space indenting

--- src/app/epubzip.hs
+++ src/app/epubzip.hs
@@ -17,52 +17,52 @@ import EpubTools.EpubZip.Opts (DirOrName (..),
 
 main :: IO ()
 main = do
-   -- No buffering, it messes with the order of output
-   mapM_ (flip hSetBuffering NoBuffering) [ stdout, stderr ]
+  -- No buffering, it messes with the order of output
+  mapM_ (flip hSetBuffering NoBuffering) [ stdout, stderr ]
 
-   either exitWith exitWith =<< (runExceptT $ do
-      opts <- liftIO parseOpts
+  either exitWith exitWith =<< (runExceptT $ do
+    opts <- liftIO parseOpts
 
-      let (DirOrName inputPath) = dirOrName opts
-      isDir <- liftIO $ doesDirectoryExist inputPath
+    let (DirOrName inputPath) = dirOrName opts
+    isDir <- liftIO $ doesDirectoryExist inputPath
 
-      en <- if isDir
+    en <- if isDir
             then do
-               let dos = EN.defaultOptions
-               fs <- initialize dos
-               runExceptT $ do
-                  xml <- snd `fmap` getPkgPathXmlFromDir "."
-                  pkg <- getPackage xml
-                  metadata <- getMetadata xml
+              let dos = EN.defaultOptions
+              fs <- initialize dos
+              runExceptT $ do
+                xml <- snd `fmap` getPkgPathXmlFromDir "."
+                pkg <- getPackage xml
+                metadata <- getMetadata xml
 
-                  (_, newPath) <- tryFormatting
-                     (Globals dos pkg metadata) fs
-                     "CURRENT DIRECTORY"
+                (_, newPath) <- tryFormatting
+                  (Globals dos pkg metadata) fs
+                  "CURRENT DIRECTORY"
 
-                  return $ inputPath </> newPath
+                return $ inputPath </> newPath
             else return . Right $ inputPath
 
-      case en of
-         Right zipPath -> do
-            exists <- liftIO $ doesFileExist zipPath
+    case en of
+      Right zipPath -> do
+        exists <- liftIO $ doesFileExist zipPath
 
-            case ((overwrite opts), exists) of
-               (Overwrite False, True) -> do
-                  _ <- liftIO $ printf
-                     "File %s exists, use --overwrite to force\n"
-                     zipPath
-                  throwError $ ExitFailure 1
-               (Overwrite True,  True) -> liftIO $ removeFile zipPath
-               (_             ,  _   ) -> return ()
-
-            _ <- liftIO $ do
-               archive <- mkEpubArchive "."
-               writeArchive zipPath archive
-               printf "Book created: %s\n" zipPath
-
-            return ExitSuccess
-
-         Left errorMsg -> do
-            liftIO $ putStrLn errorMsg
+        case ((overwrite opts), exists) of
+          (Overwrite False, True) -> do
+            _ <- liftIO $ printf
+              "File %s exists, use --overwrite to force\n"
+              zipPath
             throwError $ ExitFailure 1
-      )
+          (Overwrite True,  True) -> liftIO $ removeFile zipPath
+          (_             ,  _   ) -> return ()
+
+        _ <- liftIO $ do
+          archive <- mkEpubArchive "."
+          writeArchive zipPath archive
+          printf "Book created: %s\n" zipPath
+
+        return ExitSuccess
+
+      Left errorMsg -> do
+        liftIO $ putStrLn errorMsg
+        throwError $ ExitFailure 1
+    )
